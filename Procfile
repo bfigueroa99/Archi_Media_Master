c38beeefc4bad9de9c5563608e109ec6aa3581ed
Benjamin Figueroa Guzman <bfigueroa@miuandes.cl>
2024-11-27 19:41:09 -0300

    Update Procfile and add static files for admin interface

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-cd media_master && python manage.py runserver
+web: cd media_master && python manage.py runserver
 
--- media_masters/backend/settings.py
+++ media_masters/backend/settings.py
@@ -114,11 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# The directory where collectstatic will collect static files for deployment
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Additional locations of static files
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media_masters/static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'media_masters/staticfiles')
