8d47955c108e2c08d06b1bc5207559b2d464269d
Benjamin Figueroa Guzman <bfigueroa@miuandes.cl>
2024-11-27 19:23:32 -0300

    Add static file settings for deployment

--- media_masters/backend/settings.py
+++ media_masters/backend/settings.py
@@ -114,3 +114,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# The directory where collectstatic will collect static files for deployment
+STATIC_ROOT = BASE_DIR / "staticfiles"
