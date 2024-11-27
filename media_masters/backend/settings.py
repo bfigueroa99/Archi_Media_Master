f5bd36af2497d5a3797605b76baa2215dc587ab0
Benjamin Figueroa Guzman <bfigueroa@miuandes.cl>
2024-11-27 20:39:35 -0300

    Refactor database settings in settings.py to support SQLite for development and PostgreSQL for production

--- media_masters/backend/settings.py
+++ media_masters/backend/settings.py
@@ -15,20 +15,25 @@ from pathlib import Path
 import dj_database_url
 from decouple import config
 
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
 }
 
+# Use PostgreSQL in production
+if config('DATABASE_URL', default=None):
+    DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-rqy*(yw(cs0_o_ydymwhi)&dko7b5@6&+ep9_3e#7l-4umlgjp"
@@ -81,20 +86,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
-}
-
-# Use PostgreSQL in production
-if config('DATABASE_URL', default=None):
-    DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
@@ -129,8 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
